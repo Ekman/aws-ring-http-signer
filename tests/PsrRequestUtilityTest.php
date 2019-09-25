389bee661bd7c0e2d7e430c9901a9022f3e34f5e
Niklas Ekman <niklas@stockfiller.com>
2019-09-25 10:05:16 +0200

    Do not read the entire body into memory

--- tests/PsrRequestUtilityTest.php
+++ tests/PsrRequestUtilityTest.php
@@ -3,6 +3,7 @@ namespace Nekman\AwsRingHttpSigner\Test;
 
 use Nekman\AwsRingHttpSigner\PsrRequestUtility;
 use PHPUnit\Framework\TestCase;
+use GuzzleHttp\Stream\Stream;
 
 class PsrRequestUtilityTest extends TestCase
 {
@@ -72,6 +73,57 @@ class PsrRequestUtilityTest extends TestCase
                     "headers" => ["content-type" => ["application/json"]]
                 ],
                 null
+            ]
+        ];
+    }
+
+    /** @dataProvider provideGetBody */
+    public function testGetBody($request, $expected)
+    {
+        $body = PsrRequestUtility::getBody($request);
+
+        if (is_resource($body)) {
+            $body = stream_get_contents($body);
+        }
+
+        $this->assertEquals($expected, $body);
+    }
+
+    public function provideGetBody()
+    {
+        return [
+            "Test null" => [
+                [],
+                null
+            ],
+            "Test \GuzzleHttp\Stream\StreamInterface" => [
+                [
+                    "body" => Stream::factory("Hello, World")
+                ],
+                "Hello, World"
+            ],
+            "Test resource" => [
+                [
+                    "body" => fopen('data://text/plain,Hello World','r')
+                ],
+                "Hello World"
+            ],
+            "Test __toString" => [
+                [
+                    "body" => new class {
+                        public function __toString()
+                        {
+                            return "Hello, World";
+                        }
+                    }
+                ],
+                "Hello, World"
+            ],
+            "Test \Iterator" => [
+                [
+                    "body" => new \ArrayIterator([ "Hello", ", ", "World" ])
+                ],
+                "Hello, World"
             ]
         ];
     }
